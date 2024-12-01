c8fcc965500c63c3637454072b46f080fb9ed2b9
lodi <lodi.dev@outlook.com>
2024-11-30 20:21:37 -0600

    fix error

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from 'express';
-import slug from 'slug';
 import User from '../models/Users';
 import { hashPassword } from '../utils/auth';
 
@@ -8,19 +7,32 @@ export const createAccount = async (
   res: Response
 ): Promise<void> => {
   try {
-    const { email, password, handle } = req.body;
+    const { email, password } = req.body;
+
+    // Cargar dinámicamente el módulo slug
+    const { default: slug } = await import('slug');
+
     const userExists = await User.findOne({ email });
     if (userExists) {
       res.status(409).json({ error: 'El usuario ya está registrado' });
       return;
     }
 
+    const handle = slug(req.body.handle, '_');
+
+    const handleExists = await User.findOne({ handle });
+    if (handleExists) {
+      res
+        .status(409)
+        .json({ error: 'El nombre de usuario no está disponible' });
+      return;
+    }
+
     const user = new User(req.body);
     user.password = await hashPassword(password);
-    user.handle = slug(handle, '_');
+    user.handle = handle;
 
     await user.save();
-    //finalizar registro
     res.status(201).send({ mensaje: 'Datos recibidos correctamente' });
   } catch (error) {
     res.status(500).json({ error: 'Error del Servidor' });
