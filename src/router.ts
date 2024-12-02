335631d4443292f689f05e3ef8bf5378078c6d26
lodi <lodi.dev@outlook.com>
2024-12-02 16:10:04 -0600

    w

--- src/router.ts
+++ src/router.ts
@@ -4,11 +4,10 @@ import User from './models/Users';
 const router = Router();
 
 /** Auth and Register */
-router.post('/auth/register', (req, res) => {
+router.post('/auth/register', async (req, res) => {
   console.log('Body recibido:', req.body);
   res.status(200).send({ mensaje: 'Datos recibidos correctamente' });
-  //await User.create(req.body);
+  await User.create(req.body);
 });
 
-//
 export default router;
