d34e8d4a8d1fa284e49d6fa6586ca5aede8d1d10
lodi <lodi.dev@outlook.com>
2024-12-02 17:56:12 -0600

    update and add validator

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,3 @@ const port = process.env.PORT || 4000;
 server.listen(port, () => {
   console.log(colors.green.bold(`SERVER port: ${port}`));
 });
-
-let productName = 'Table';
-let isAuth = false;
-let price = 30;
-
-type Product = {
-  id: number;
-  price: number;
-  name: string;
-};
--- src/router.ts
+++ src/router.ts
@@ -1,13 +1,17 @@
 import { Router } from 'express';
+import { body } from 'express-validator';
 import { createAccount } from './handlers';
 
 const router = Router();
 
 /** Auth and Register */
-router.post('/auth/register', async (req, res) => {
-  console.log('Body recibido:', req.body);
-  res.status(200).send({ mensaje: 'Datos recibidos correctamente' });
-  await User.create(req.body);
-});
+router.post(
+  '/auth/register',
+  body('handle').notEmpty().withMessage('El handle no puede ir vacio'),
+  body('name').notEmpty().withMessage('El nombre no puede ir vacio'),
+  body('email').isEmail().withMessage('El email no puede ir vacio'),
+  body('password').isLength({ min: 8 }).withMessage('El password es muy corto'),
+  createAccount
+);
 
 export default router;
