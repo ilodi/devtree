66e41e2338b8d81c2d732ee01199e09586cf801a
lodi <lodi.dev@outlook.com>
2024-12-04 17:04:55 -0600

    update

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 import 'dotenv/config';
 import router from './router';
 import { connectDB } from './config/db';
+import { corsConfig } from './config/cors';
 
 const app = express();
 connectDB();
+
+// Cors
+app.use(cors(corsConfig));
 
 //leer datos de formularios
 app.use(express.json());
